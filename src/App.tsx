44856b1785035c0ed03fbf1d2ca11717928a1716
Stepan Klos <stepanklos@gmail.com>
2022-12-22 22:43:34 +0100

    Axios added to project+included

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row } from 'reactstrap';
 import './App.css';
+import axios from 'axios';
 
 function App() {
   return (
