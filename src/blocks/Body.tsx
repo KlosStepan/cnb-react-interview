6bf85397cda42fec94d19732670d7b53de22bfe1
Stepan Klos <stepanklos@gmail.com>
2022-12-23 14:52:46 +0100

    Table for currencies prep

--- src/blocks/Body.tsx
+++ src/blocks/Body.tsx
@@ -1,7 +1,26 @@
-import { Row } from 'reactstrap';
+import { Row, Table } from 'reactstrap';
+import TRow from '../components/TRow';
 function Body() {
     return (<Row>
-        <span>body goes here</span>
+        <span>|body|</span>
+        <Table striped hover size="sm">
+            <thead>
+                <tr>
+                    <th>1</th>
+                    <th>2</th>
+                    <th>3</th>
+                    <th>4</th>
+                    <th>5</th>
+                </tr>
+            </thead>
+            <tbody>
+                <TRow />
+                <TRow />
+                <TRow />
+                <TRow />
+                <TRow />
+            </tbody>
+        </Table>
     </Row>)
 }
 export default Body
--- src/components/TRow.tsx
+++ src/components/TRow.tsx
@@ -1,5 +1,11 @@
 import { Row } from 'reactstrap';
 function TRow() {
-    return (<Row>|t row|</Row>)
+    return (<tr>
+        <td>asdfghjkl</td>
+        <td>asdfghjkl</td>
+        <td>asdfghjkl</td>
+        <td>asdfghjkl</td>
+        <td>asdfghjkl</td>
+    </tr>)
 }
 export default TRow
